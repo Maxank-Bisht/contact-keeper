329e717a42ec04a43a0608c419f90da19c57157e
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-14 21:01:31 +0530

    define routes

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKeeper API' }));
+
+//routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
