a7c1f8d91e84ac4e6255faa6a0509d6c47e7e487
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-15 17:14:57 +0530

    hashed password and saved to DB

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -18,10 +19,30 @@ router.post(
 		// password should be greater than 6 character
 		check('password', 'Password should have 6 or more characters').isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return res.status(400).json({ msg: 'user already exists' });
+			}
+			user = new User({ name, email, password });
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			res.send('user saved');
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('server error');
 		}
 	}
 );
