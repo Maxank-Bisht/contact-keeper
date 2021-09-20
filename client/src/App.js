5e9605fb0a80965a6c390fcfbe620f019cf4bd17
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-20 21:20:39 +0530

    created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 const App = () => {
 	return (
 		<div className='App'>
-			<h1>Contact Keeper</h1>
+			<Navbar />
 		</div>
 	);
 };
