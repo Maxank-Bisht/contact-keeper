8d79faae0bdb559af1bea73ffc184647fc460c26
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-29 18:28:43 +0530

    add functionality created

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -41,6 +41,10 @@ const ContactState = (props) => {
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
 	//add contact
+	const addContact = (contact) => {
+		contact.id = uuid();
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
 
 	//delete contact
 
@@ -54,7 +58,11 @@ const ContactState = (props) => {
 
 	// clear filter
 
-	return <ContactContext.Provider value={{ contacts: state.contacts }}>{props.children}</ContactContext.Provider>;
+	return (
+		<ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
+			{props.children}
+		</ContactContext.Provider>
+	);
 };
 
 export default ContactState;
