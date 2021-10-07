545e9409759c60dbe392cf8ef3d4aecea61ec356
Mayank Bisht <bishtmayank799@gmail.com>
2021-10-07 16:55:46 +0530

    create alert context

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -41,22 +41,27 @@ const ContactForm = () => {
 	};
 
 	return (
-		<form onSubmit={onSubmit}>
+		<form style={myStyle} onSubmit={onSubmit}>
 			<h2 className='text-primary'> {current ? 'Edit Contact' : 'Add Contact'} </h2>
 			<input type='text' name='name' value={name} placeholder='Name' onChange={onChange} />
 			<input type='email' name='email' value={email} placeholder='Email' onChange={onChange} />
 			<input type='text' name='phone' value={phone} placeholder='Phone' onChange={onChange} />
-			<h5>Contact Type:</h5>
-			<input type='radio' name='type' value='personal' onChange={onChange} checked={type === 'personal'} />
-			Personal{' '}
-			<input
-				type='radio'
-				name='type'
-				value='professional'
-				onChange={onChange}
-				checked={type === 'professional'}
-			/>
-			Professional
+			<h4>Contact Type:</h4>
+			<label className='myRadio'>
+				<input type='radio' name='type' value='personal' onChange={onChange} checked={type === 'personal'} />
+				<span>Personal</span>
+			</label>
+			<label className='myRadio'>
+				<input
+					type='radio'
+					name='type'
+					value='professional'
+					onChange={onChange}
+					checked={type === 'professional'}
+				/>
+				<span>Professional</span>
+			</label>
+
 			<div>
 				<input
 					type='submit'
@@ -73,6 +78,14 @@ const ContactForm = () => {
 			)}
 		</form>
 	);
+};
+
+const myStyle = {
+	boxShadow: '0px 4px 8px #00000047',
+	padding: '2rem',
+	marginTop: '2rem',
+	marginBottom: '2rem',
+	borderRadius: '7px',
 };
 
 export default ContactForm;
